85f2b4aec3c8857639ba3a87484bdfad0db4f725
Valentin Palkovic <valentin@chromatic.com>
2026-02-13 10:49:49 +0100

    Web-Components/Vue3: Update core presets to extend existing config

--- packages/framework-web-components/src/preset.ts
+++ packages/framework-web-components/src/preset.ts
@@ -2,10 +2,11 @@ import { fileURLToPath } from 'node:url'
 import type { PresetProperty } from 'storybook/internal/types'
 import type { StorybookConfig } from './types'
 
-export const core: PresetProperty<'core'> = async (_config, options) => {
+export const core: PresetProperty<'core'> = async (config, options) => {
   const framework = await options.presets.apply('framework')
 
   return {
+    ...config,
     builder: {
       name: fileURLToPath(import.meta.resolve('storybook-builder-rsbuild')),
       options:
